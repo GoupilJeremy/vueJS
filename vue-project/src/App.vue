1980ea5eac89896746b64f24c7e6fb3cbd03f3bb
Jérémy Goupil <goupiljeremy@gmail.com>
2024-12-02 10:08:12 +0000

    feat: ✨list

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,10 +2,17 @@
 import { ref } from 'vue'
 
 const header = ref('Shopping List App')
+const items = ref([
+    {id: 1, label: "10 party hats"},
+    {id: 2, label: "2 board games"},
+    {id: 3, label: "20 cups"}
+])
 </script>
 
 <template>
  <h1>{{ header }}</h1>
- <input v-model="msg">
+ <ul>
+    <li v-for="{id, label} in items" :key="id">{{ label }}</li>
+ </ul>
 </template>
 
